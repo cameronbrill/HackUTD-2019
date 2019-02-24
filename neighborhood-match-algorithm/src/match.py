c991320d394a855850c35191d87651a1d6a859f4
gaiscioch <c00brill@gmail.com>
2019-02-24 12:25:39 +0000

    Added python code to neighborhoood match algorithm

--- neighborhood-match-algorithm/src/match.py
+++ neighborhood-match-algorithm/src/match.py
@@ -1,3 +1,15 @@
-import requests
+import requests as r
+
+omg = r.post('http://10.142.0.2:3000/neighborhood', data = {'name': "Cameron", 'ethnicity': "White", 'age': 18, 'sex': "Male", 'travel': "no", 'travel_time':"Once a day", 'num_adults':6, 'num_children':82, 'health':"good", 'education_level': "College", 'address': "Penis", 'household_income': 820000, 'credit_score':850})
 
 
+
+
+got = r.get('http://10.142.0.2:3000/neighborhood')
+
+print(got.status_code)
+
+print(got.headers['content-type'])
+print(got.encoding)
+print(got.text)
+print(got.json())
